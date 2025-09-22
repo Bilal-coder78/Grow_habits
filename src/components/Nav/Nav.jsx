0d398637a5e0dd673c878795823817041b27b4ad
M Bilal <bilalsikander87@gmail.com>
2025-09-22 18:52:56 +0500

    left nav com

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='container'>
+            <div class="d-flex justify-content-between align-items-center mt-3">
+                <div class="d-flex align-items-center gap-1">
+                    <div class="py-2 px-3 rounded-3 fs-4 fw-bold text-white d-flex justify-content-center align-items-center" style={{ background: "linear-gradient(135deg, var(--primary-500), var(--warm-500))" }}>G</div>
+                    <div class="content ms-2">
+                        <h1 className='fs-5 m-0'>GrowHabits</h1>
+                        <div className='small text-muted'>Grow healthy • Be kind • Learn daily</div>
+                    </div>
+                </div>
+                <div class="nav-items">hhj</div>
+                <div class="buttons">byyy</div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
